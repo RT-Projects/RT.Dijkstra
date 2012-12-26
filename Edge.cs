ff544e030bcbb4ac616943b9e583a5133c98f2b9
Timwi <github@timwi.de>
2012-12-26 20:23:01 +0100

    Documentation (and a few renames)
    --HG--
    rename : Move.cs => Edge.cs

---
+++ Edge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT.Util
+{
+    /// <summary>Encapsulates an edge in the graph for breadth-first search, containing a label and a target node.</summary>
+    /// <typeparam name="TLabel">Type that can be used to identify this edge.</typeparam>
+    public sealed class Edge<TLabel>
+    {
+        /// <summary>The label on this edge.</summary>
+        public TLabel Label { get; private set; }
+
+        /// <summary>The node this edge points to.</summary>
+        public Node<TLabel> Node { get; private set; }
+
+        /// <summary>Initializes a new instance of <see cref="Edge{TLabel}"/>.</summary>
+        /// <param name="label">The label on this edge.</param>
+        /// <param name="node">The node this edge points to.</param>
+        public Edge(TLabel label, Node<TLabel> node)
+        {
+            Label = label;
+            Node = node;
+        }
+    }
+}
--- TreeNode.cs
+++ TreeNode.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace RT.Util
 {
-    public abstract class TreeNode<TMove> : IEquatable<TreeNode<TMove>>
+    /// <summary>Base class for nodes in a graph on which breadth-first search is performed by <see cref="BreadthFirstSearch.Run"/>.</summary>
+    /// <typeparam name="TLabel">Type that can be used to identify edges connecting nodes.</typeparam>
+    public abstract class Node<TLabel> : IEquatable<Node<TLabel>>
     {
-        public abstract bool Equals(TreeNode<TMove> other);
+        /// <summary>When overridden in a derived class, compares two nodes for equality.</summary>
+        public abstract bool Equals(Node<TLabel> other);
+
+        /// <summary>When overridden in a derived class, returns a hash code for this node.</summary>
         public abstract override int GetHashCode();
 
+        /// <summary>
+        /// When overridden in a derived class, determines whether this node is “final”. The final nodes are
+        /// the nodes the breadth-first search is looking for. The first such node encountered in the search
+        /// ends the search with success.
+        /// </summary>
         public abstract bool IsFinal { get; }
-        public abstract IEnumerable<Edge<TMove>> Edges { get; }
+
+        /// <summary>
+        /// When overridden in a derived class, returns the set of edges going out from this node.
+        /// Each edge consists of a label that identifies the edge, and the node that the edge points to.
+        /// </summary>
+        public abstract IEnumerable<Edge<TLabel>> Edges { get; }
     }
 }
