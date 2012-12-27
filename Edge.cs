79aec61c38c7ee81145ee389ee3ab8af040368f1
Timwi <github@timwi.de>
2012-12-27 01:28:01 +0100

    Generalize to Dijkstra's Algorithm, and rename everything to Dijkstra
    --HG--
    rename : BreadthFirstSearch.cs => Dijkstra.cs
    rename : BreadthFirstSearch.csproj => Dijkstra.csproj
    rename : BreadthFirstSearch.sln => Dijkstra.sln
    rename : TreeNode.cs => Node.cs

--- Edge.cs
+++ Edge.cs
@@ -6,21 +6,27 @@ using System.Threading.Tasks;
 
 namespace RT.Util
 {
-    /// <summary>Encapsulates an edge in the graph for breadth-first search, containing a label and a target node.</summary>
+    /// <summary>Encapsulates an edge in the graph for Dijkstra’s Algorithm, containing a weight, a label and a target node.</summary>
+    /// <typeparam name="TWeight">Type of the weight of each edge.</typeparam>
     /// <typeparam name="TLabel">Type that can be used to identify this edge.</typeparam>
-    public sealed class Edge<TLabel>
+    public sealed class Edge<TWeight, TLabel>
     {
+        /// <summary>The weight of this edge.</summary>
+        public TWeight Weight { get; private set; }
+
         /// <summary>The label on this edge.</summary>
         public TLabel Label { get; private set; }
 
         /// <summary>The node this edge points to.</summary>
-        public Node<TLabel> Node { get; private set; }
+        public Node<TWeight, TLabel> Node { get; private set; }
 
         /// <summary>Initializes a new instance of <see cref="Edge{TLabel}"/>.</summary>
         /// <param name="label">The label on this edge.</param>
+        /// <param name="weight">The weight of this edge.</param>
         /// <param name="node">The node this edge points to.</param>
-        public Edge(TLabel label, Node<TLabel> node)
+        public Edge(TWeight weight, TLabel label, Node<TWeight, TLabel> node)
         {
+            Weight = weight;
             Label = label;
             Node = node;
         }
--- Node.cs
+++ Node.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace RT.Util
 {
-    /// <summary>Base class for nodes in a graph on which breadth-first search is performed by <see cref="BreadthFirstSearch.Run"/>.</summary>
+    /// <summary>Base class for nodes in a graph on which breadth-first search is performed by <see cref="Dijkstra.Run"/>.</summary>
     /// <typeparam name="TLabel">Type that can be used to identify edges connecting nodes.</typeparam>
-    public abstract class Node<TLabel> : IEquatable<Node<TLabel>>
+    public abstract class Node<TWeight, TLabel> : IEquatable<Node<TWeight, TLabel>>
     {
         /// <summary>When overridden in a derived class, compares two nodes for equality.</summary>
-        public abstract bool Equals(Node<TLabel> other);
+        public abstract bool Equals(Node<TWeight, TLabel> other);
 
         /// <summary>When overridden in a derived class, returns a hash code for this node.</summary>
         public abstract override int GetHashCode();
@@ -27,6 +27,6 @@ namespace RT.Util
         /// When overridden in a derived class, returns the set of edges going out from this node.
         /// Each edge consists of a label that identifies the edge, and the node that the edge points to.
         /// </summary>
-        public abstract IEnumerable<Edge<TLabel>> Edges { get; }
+        public abstract IEnumerable<Edge<TWeight, TLabel>> Edges { get; }
     }
 }
