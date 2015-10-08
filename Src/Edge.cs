e8b8d26840ddf42678dcfdd350e34e7a84b37b59
Timwi <github@timwi.de>
2015-10-09 00:12:20 +0200

    Restructure, add README, add .gitignore

---
+++ Src/Edge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT.Dijkstra
+{
+    /// <summary>Encapsulates an edge in the graph for Dijkstra’s Algorithm, containing a weight, a label and a target node.</summary>
+    /// <typeparam name="TWeight">Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
+    /// <typeparam name="TLabel">Type that can be used to identify this edge.</typeparam>
+    public sealed class Edge<TWeight, TLabel>
+    {
+        /// <summary>The weight of this edge. Dijkstra’s Algorithm finds the path with the smallest total weight.</summary>
+        public TWeight Weight { get; private set; }
+
+        /// <summary>The label on this edge.</summary>
+        public TLabel Label { get; private set; }
+
+        /// <summary>The node this edge points to.</summary>
+        public Node<TWeight, TLabel> Node { get; private set; }
+
+        /// <summary>Initializes a new instance of <see cref="Edge{TWeight,TLabel}"/>.</summary>
+        /// <param name="weight">The weight of this edge.</param>
+        /// <param name="label">The label on this edge.</param>
+        /// <param name="node">The node this edge points to.</param>
+        public Edge(TWeight weight, TLabel label, Node<TWeight, TLabel> node)
+        {
+            Weight = weight;
+            Label = label;
+            Node = node;
+        }
+    }
+}
---
+++ Src/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT.Dijkstra
+{
+    /// <summary>Base class for nodes in a graph on which breadth-first search is performed by <see cref="Dijkstra.Run"/>.</summary>
+    /// <typeparam name="TWeight">Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
+    /// <typeparam name="TLabel">Type that can be used to identify edges connecting nodes.</typeparam>
+    public abstract class Node<TWeight, TLabel> : IEquatable<Node<TWeight, TLabel>>
+    {
+        /// <summary>When overridden in a derived class, compares two nodes for equality.</summary>
+        public abstract bool Equals(Node<TWeight, TLabel> other);
+
+        /// <summary>When overridden in a derived class, returns a hash code for this node.</summary>
+        public abstract override int GetHashCode();
+
+        /// <summary>
+        /// When overridden in a derived class, determines whether this node is “final”. The final nodes are
+        /// the nodes the breadth-first search is looking for. The first such node encountered in the search
+        /// ends the search with success.
+        /// </summary>
+        public abstract bool IsFinal { get; }
+
+        /// <summary>
+        /// When overridden in a derived class, returns the set of edges going out from this node.
+        /// Each edge consists of a label that identifies the edge, and the node that the edge points to.
+        /// </summary>
+        public abstract IEnumerable<Edge<TWeight, TLabel>> Edges { get; }
+    }
+}
