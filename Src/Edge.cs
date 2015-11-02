72515838b07583a740c385d9c0333673ebf20f64
Timwi <github@timwi.de>
2015-11-02 18:06:28 +0100

    XML documentation.

--- Src/Edge.cs
+++ Src/Edge.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace RT.Dijkstra
 {
-    /// <summary>Encapsulates an edge in the graph for Dijkstra’s Algorithm, containing a weight, a label and a target node.</summary>
-    /// <typeparam name="TWeight">Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
-    /// <typeparam name="TLabel">Type that can be used to identify this edge.</typeparam>
+    /// <summary>
+    ///     Encapsulates an edge in the graph for Dijkstra’s Algorithm, containing a weight, a label and a target node.</summary>
+    /// <typeparam name="TWeight">
+    ///     Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
+    /// <typeparam name="TLabel">
+    ///     Type that can be used to identify this edge.</typeparam>
     public sealed class Edge<TWeight, TLabel>
     {
         /// <summary>The weight of this edge. Dijkstra’s Algorithm finds the path with the smallest total weight.</summary>
@@ -20,10 +23,14 @@ namespace RT.Dijkstra
         /// <summary>The node this edge points to.</summary>
         public Node<TWeight, TLabel> Node { get; private set; }
 
-        /// <summary>Initializes a new instance of <see cref="Edge{TWeight,TLabel}"/>.</summary>
-        /// <param name="weight">The weight of this edge.</param>
-        /// <param name="label">The label on this edge.</param>
-        /// <param name="node">The node this edge points to.</param>
+        /// <summary>
+        ///     Initializes a new instance of <see cref="Edge{TWeight,TLabel}"/>.</summary>
+        /// <param name="weight">
+        ///     The weight of this edge.</param>
+        /// <param name="label">
+        ///     The label on this edge.</param>
+        /// <param name="node">
+        ///     The node this edge points to.</param>
         public Edge(TWeight weight, TLabel label, Node<TWeight, TLabel> node)
         {
             Weight = weight;
--- Src/Node.cs
+++ Src/Node.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace RT.Dijkstra
 {
-    /// <summary>Base class for nodes in a graph on which breadth-first search is performed by <see cref="Dijkstra.Run"/>.</summary>
-    /// <typeparam name="TWeight">Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
-    /// <typeparam name="TLabel">Type that can be used to identify edges connecting nodes.</typeparam>
+    /// <summary>
+    ///     Base class for nodes in a graph on which breadth-first search is performed by <see
+    ///     cref="DijkstrasAlgorithm.Run"/>.</summary>
+    /// <typeparam name="TWeight">
+    ///     Type of the weight (or length or any other quantity to be minimized) of each edge between nodes.</typeparam>
+    /// <typeparam name="TLabel">
+    ///     Type that can be used to identify edges connecting nodes.</typeparam>
     public abstract class Node<TWeight, TLabel> : IEquatable<Node<TWeight, TLabel>>
     {
         /// <summary>When overridden in a derived class, compares two nodes for equality.</summary>
@@ -18,16 +22,14 @@ namespace RT.Dijkstra
         public abstract override int GetHashCode();
 
         /// <summary>
-        /// When overridden in a derived class, determines whether this node is “final”. The final nodes are
-        /// the nodes the breadth-first search is looking for. The first such node encountered in the search
-        /// ends the search with success.
-        /// </summary>
+        ///     When overridden in a derived class, determines whether this node is “final”. The final nodes are the nodes the
+        ///     breadth-first search is looking for. The first such node encountered in the search ends the search with
+        ///     success.</summary>
         public abstract bool IsFinal { get; }
 
         /// <summary>
-        /// When overridden in a derived class, returns the set of edges going out from this node.
-        /// Each edge consists of a label that identifies the edge, and the node that the edge points to.
-        /// </summary>
+        ///     When overridden in a derived class, returns the set of edges going out from this node. Each edge consists of a
+        ///     label that identifies the edge, and the node that the edge points to.</summary>
         public abstract IEnumerable<Edge<TWeight, TLabel>> Edges { get; }
     }
 }
