26e9fa982751892987c741a9c703c41af3cc1195
Timwi <github@timwi.de>
2020-05-19 01:38:08 +0200

    Some cleanup; reduce .NET Framework version number

--- Src/Edge.cs
+++ Src/Edge.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RT.Dijkstra
+﻿namespace RT.Dijkstra
 {
     /// <summary>
     ///     Encapsulates an edge in the graph for Dijkstra’s Algorithm, containing a weight, a label and a target node.</summary>
